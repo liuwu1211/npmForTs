81d54f10490d876bf2cee907867292d2cfe89b30
liuwufangzhou <liuwufangzhou@smzdm.com>
2020-03-12 18:27:56 +0800

    feat: 更新了打包与压缩

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,5 +1,9 @@
 import typescript from 'rollup-plugin-typescript2';
 import path from 'path';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser'
  
 export default {
   input: 'src/index.ts',
@@ -15,5 +19,20 @@ export default {
         format: 'esm'
     }
 ],
-  plugins: [typescript()]
+  plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      terser({
+        output: {
+            ascii_only: true // 仅输出ascii字符
+        }
+        // compress: {
+        //     pure_funcs: ['console.log'] // 去掉console.log函数
+        // }
+    })
+  ]
 };
